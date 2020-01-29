e0ae1f9676c873625e5f41fa9b2b5402d986f6e1
Kaballah <kabzkabala4@gmail.com>
2020-01-29 12:16:28 +0300

    examine data types in JS

---
+++ data_types.js
@@ -0,0 +1,25 @@
+// Scope Binding; var, let, const 
+console.log('Understanding var'); 
+var name; //variable delaration
+name = 'Ronnie Kabalah'; //variable assignment of type 'String'
+console.log(name);
+var name = 'Minace McKaballah';
+console.log(name);
+
+
+console.log('Understanding let');
+let fruit; 
+fruit = 'apple';
+console.log(fruit);
+fruit = 'pineapple';
+console.log(fruit);
+// let fruit;
+// fruit = 'peach';
+// console.log(fruit);
+
+// Const in action
+const PI = 3.142;
+console.log(PI);
+// Expect an error
+// PI = 2.9
+// console.log(PI);
